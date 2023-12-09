c67e655486973f0060b0f8f9dda7cfeda61d20d1
Eduardo Adame <eadamesalles@gmail.com>
2023-12-09 14:20:40 -0300

    Fixing #12 merge

--- cnpj/views.py
+++ cnpj/views.py
@@ -53,9 +53,9 @@ def analysis(request, pk):
         'estabelecimentos': estabelecimentos,
     }
 
-    # if request.user.is_authenticated:
-    #     request.user.account.search_history.append(empresas.cnpj_basico)
-    #     request.user.account.save()
+    if request.user.is_authenticated:
+        request.user.account.search_history.append(empresas.cnpj_basico)
+        request.user.account.save()
     
     return render(request, 'details.html', context)
 
@@ -78,28 +78,28 @@ def profile(response):
 
     if response.user.is_authenticated:
         search_history = response.user.account.search_history
-        estabelecimentos = Estabelecimentos.objects.filter(cnpj_basico_id__in=search_history)
+        empresas = Empresas.objects.filter(cnpj_basico__in=search_history)
         
-    return render(response, "profile.html", {"estabelecimentos":estabelecimentos})
+    return render(response, "profile.html", {"empresas":empresas})
 
 def search_compare(request, pk):
-    first_estabelecimento = Estabelecimentos.objects.filter(cnpj_basico_id=pk)[0]
+    first_empresa = Empresas.objects.filter(cnpj_basico=pk)[0]
     context = {
-        'first_estabelecimento': first_estabelecimento,
+        'first_empresa': first_empresa,
     }
     if request.GET.get('q'):
         query = request.GET.get('q')
-        estabelecimentos_list = Estabelecimentos.objects.filter( Q(cnpj_basico_id__icontains=query) | Q(nome_fantasia__icontains=query) )
-        context['estabelecimentos_list'] = estabelecimentos_list
+        empresas_list = Empresas.objects.filter( Q(cnpj_basico__icontains=query) | Q(razao_social__icontains=query) )
+        context['empresas_list'] = empresas_list
     
     
     return render(request, 'search_compare.html', context)
 
 def comparision(request, pk, pk2):
-    first_estabelecimento = Estabelecimentos.objects.filter(cnpj_basico_id=pk)[0]
-    second_estabelecimento = Estabelecimentos.objects.filter(cnpj_basico_id=pk2)[0]
+    first_empresa = Empresas.objects.filter(cnpj_basico=pk)[0]
+    second_empresa = Empresas.objects.filter(cnpj_basico=pk2)[0]
     context = {
-        'first_estabelecimento': first_estabelecimento,
-        'second_estabelecimento': second_estabelecimento,
+        'first_empresa': first_empresa,
+        'second_empresa': second_empresa,
     }
     return render(request, 'comparision.html', context)
